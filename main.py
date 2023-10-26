1aaa11c55a9866ddb86c0c14efded378ff0c6c69
jadepaoletta <31874306+jadepaoletta@users.noreply.github.com>
2023-10-26 15:05:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import getpass
 
 print("whoami = " + getpass.getuser())
 
-cwd = os.getcwd()
-print(cwd)
+# cwd = os.getcwd()
+# print(cwd)
 
-os.chdir("/opt/render/.cache/")
-cwd = os.getcwd()
-print(cwd)
+# os.chdir("/opt/render/.cache/")
+# cwd = os.getcwd()
+# print(cwd)
 
 f = open("myfile.txt", "x")
 f.write("THIS IS A TEST")
