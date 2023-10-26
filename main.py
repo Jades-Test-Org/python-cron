737792d263be5c4ed81b66b913529210a435d257
jadepaoletta <31874306+jadepaoletta@users.noreply.github.com>
2023-10-26 14:48:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ print("whoami = " + getpass.getuser())
 cwd = os.getcwd()
 print(cwd)
 
-f = open("/opt/render/.cache/myfile.txt", "x")
+os.chdir("/opt/render/.cache/")
+cwd = os.getcwd()
+print(cwd)
+
+f = open("myfile.txt", "x")
 f.write("THIS IS A TEST")
 f.close()
