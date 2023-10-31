a2051920a929e66b85e49770fa6c8ab3a9a74076
jadepaoletta <31874306+jadepaoletta@users.noreply.github.com>
2023-10-31 11:09:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-import os
-import getpass
-
-print("whoami = " + getpass.getuser())
-
-print("Current directory is" + os.getcwd())
-
-print("Changing directory to /opt/render/")
-os.chdir("/opt/render/")
-
-print("List files and directories")
-print(os.listdir())
-
-f = open("/opt/render/.cache/myfile.txt", "x")
-f.write("THIS IS A TEST")
-f.close()
+print("hello cron")
